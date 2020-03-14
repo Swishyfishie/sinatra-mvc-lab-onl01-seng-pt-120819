8e95e63f5178e3b1303f230124d71e2630af6d5f
Swishyfishie <pclandreiuk@gmail.com>
2020-03-14 05:49:18 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,62 @@
+class PigLatinizer
+
+    # def piglatinize(input)
+    #     input.split(' ').length == 1 ? piglatinize_word(input) : piglatinize_sentence(input)
+    # end
+
+    # def is_consonant?(letter)
+    #     !letter.match(/[aAeEiIoOuU]/)
+    # end
+
+    # def piglatinize_word(word) 
+    #    if is_consonant?(word[0])
+    #     word = word + "w"
+    #    elsif is_consonant?(word[0]) && is_consonant?(word[1]) && is_consonant?(word[2])
+    #     word = word.slice(3..-1) + word.slice(0,3)
+    #    elsif is_consonant?(word[0]) && is_consonant?(word[1])
+    #     word = word.slice(2..-1) + word.slice(0,2)
+    #   else
+    #     word = word.slice(1..-1) + word.slice(0)
+    #   end
+    #   word << "ay"
+    # end
+
+    # def piglatinize_sentence(sentence)
+    #     sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+    # end
+
+    
+  def piglatinize(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+  end
+
+  private
+
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
+
+  def piglatinize_word(word)
+
+    if !consonant?(word[0])
+      word = word + "w"
+
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
+
+  def piglatinize_sentence(sentence)
+    sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+  end
+
+
+
+end
